fbe2a1e920738604ca165aa5867d130743f3e480
Luis Alejandro Martínez Faneyth <luis@luisalejandro.org>
2022-02-09 06:50:52 -0400

    [REF] Updating python versions.

--- setup.py
+++ setup.py
@@ -40,10 +40,10 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     test_suite='tests',
     tests_require=read_requirements('requirements-dev.txt')
